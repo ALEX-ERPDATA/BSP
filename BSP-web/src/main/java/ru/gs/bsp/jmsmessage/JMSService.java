e0eaea2d6b6c7b0fc7b726e2d2d3b1e8e1984049
Alex <Alex@LAPG5>
2019-02-20 11:40:39 +0300

    commit1

--- BSP-web/src/main/java/ru/gs/bsp/jmsmessage/JMSService.java
+++ BSP-web/src/main/java/ru/gs/bsp/jmsmessage/JMSService.java
@@ -38,7 +38,9 @@ public class JMSService {
 
         try {
             JmsFactoryFactory ff = JmsFactoryFactory.getInstance(WMQConstants.WMQ_PROVIDER);
-            JmsConnectionFactory cf = (com.ibm.msg.client.jms.JmsConnectionFactory) ff.createConnectionFactory();
+            //JmsConnectionFactory cf = 
+            com.ibm.msg.client.jms.JmsConnectionFactory  cf =
+                    (com.ibm.msg.client.jms.JmsConnectionFactory) ff.createConnectionFactory();
             
             cf.setStringProperty(WMQConstants.WMQ_HOST_NAME, HOST);
             cf.setIntProperty(WMQConstants.WMQ_PORT, PORT);
